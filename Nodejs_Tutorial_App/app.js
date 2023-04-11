604c229e6cc5cd2f7bb9a649f9828ff31fcb58fb
Elizabeth Salako <waleade602@gmail.com>
2023-04-11 09:56:47 -0500

    sending response using jsend format

--- Nodejs_Tutorial_App/app.js
+++ Nodejs_Tutorial_App/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const port = 3000;
 const app = express();
 
 
+
 app.get("/user/:name/:age/:sex", (req, res, next) => {
-  res.json({ "user": req.params });
+  res.status(200).json({
+    "status": "success",
+    "message": "key values entered from the URL",
+    "user": req.params
+  });
+
 });
 
 
+
 //Setting up server
-const port = 3000;
+
 app.listen(port, () => {
   console.log(`Server starting on port ${port}`);
 });
