3e211e693204bf3f6700dbe0b62f1ea4e8b0872e
Elizabeth Salako <waleade602@gmail.com>
2023-04-24 16:16:43 -0500

    entry file

--- apiCalls/app.js
+++ apiCalls/app.js
@@ -7,7 +7,6 @@ const app = express();
 app.use('/', apiRouter)
 
 
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your server is live and listening on port: ${port}`);
